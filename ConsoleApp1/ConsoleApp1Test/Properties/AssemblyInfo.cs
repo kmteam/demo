bbac79eeb1622a22ad1d9846d53a72ce4396cddd
artur.nebot@kantar.com <58468934+kmspain@users.noreply.github.com>
2020-01-21 13:19:37 +0100

    Create the demo project.

---
+++ ConsoleApp1/ConsoleApp1Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsoleApp1Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp1Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee7e21fb-e935-4712-85ac-8ef4e53fec2b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ConsoleApp1/ConsoleApp1Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+
+namespace ConsoleApp1Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGreetMethod()
+        {
+
+
+            var app = new ConsoleApp1.ConsoleApp1();
+
+            var name = "Jose";
+
+            var res = app.Greet(name);
+            System.Diagnostics.Debug.Assert(res == $"{name} have a nice day!");
+
+        }
+    }
+}
